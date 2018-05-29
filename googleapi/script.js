16f15cd957565023800a6fb24fb1ec684971c2bf
ihaahjem <ingridmargit@gmail.com>
2018-05-29 22:04:56 +0200

    forsøkt endringer i infovindu

--- googleapi/script.js
+++ googleapi/script.js
@@ -6,51 +6,51 @@ var markers = [
 //
 //Skole markører
 //
-    ['Campus Fjerdingen', 59.916224, 10.759697, 0, 'school', 'sirkelmarker.png', 'http://facebook.com'],
-    ['Campus Vulkan', 59.9233391, 10.7503081, 1, 'school', 'sirkelmarker.png', 'http://facebook.com'],
-    ['Campus Brenneriveien', 59.920352, 10.7506041, 2, 'school', 'sirkelmarker.png', 'http://facebook.com'],
-    ['Campus Kvadraturen', 59.911015, 10.7439543, 3, 'school', 'sirkelmarker.png', 'http://facebook.com'],
+    ['Campus Fjerdingen', 59.916224, 10.759697, 0, 'school', 'sirkelmarker.png', 'http://facebook.com', '/infovindu-bilder/hk.jpg'],
+    ['Campus Vulkan', 59.9233391, 10.7503081, 1, 'school', 'sirkelmarker.png', 'http://facebook.com', '/infovindu-bilder/hk.jpg'],
+    ['Campus Brenneriveien', 59.920352, 10.7506041, 2, 'school', 'sirkelmarker.png', 'http://facebook.com', '/infovindu-bilder/hk.jpg'],
+    ['Campus Kvadraturen', 59.911015, 10.7439543, 3, 'school', 'sirkelmarker.png', 'http://facebook.com', '/infovindu-bilder/hk.jpg'],
 //
 //Spisested markører
 //
-    ['Talor og Jørgen', 59.9149799, 10.7504415, 4, 'food', 'food-marker.png', 'http://facebook.com'],
-    ['Mathallen', 59.922217, 10.749852, 5, 'food', 'food-marker.png', 'http://facebook.com'],
-    ['Taco República', 59.9170294, 10.7509215, 6, 'food', 'food-marker.png', 'http://facebook.com'],
-    ['Freddy Fuego', 59.9178054, 10.7515039, 7, 'food', 'food-marker.png', 'http://facebook.com'],
-    ['Peloton', 59.9174132, 10.7522924, 8, 'food', 'food-marker.png', 'http://facebook.com'],
-    ['Munchies', 59.91638, 10.7490935, 9, 'food', 'food-marker.png', 'http://facebook.com'],
-    ['Haralds Vaffler', 59.913759, 10.745212, 10, 'food', 'food-marker.png', 'http://facebook.com'],
-    ['Würst', 59.9137012, 10.7465163, 11, 'food', 'food-marker.png', 'http://facebook.com'],
-    ['Bari Pizza', 59.9166197, 10.7527744, 12, 'food', 'food-marker.png', 'http://facebook.com'],
+    ['Talor og Jørgen', 59.9149799, 10.7504415, 4, 'food', 'food-marker.png', 'http://facebook.com', '/infovindu-bilder/t&j.jpg'],
+    ['Mathallen', 59.922217, 10.749852, 5, 'food', 'food-marker.png', 'http://facebook.com', '/infovindu-bilder/t&j.jpg'],
+    ['Taco República', 59.9170294, 10.7509215, 6, 'food', 'food-marker.png', 'http://facebook.com', '/infovindu-bilder/taco-republica.jpg'],
+    ['Freddy Fuego', 59.9178054, 10.7515039, 7, 'food', 'food-marker.png', 'http://facebook.com', '/infovindu-bilder/t&j.jpg'],
+    ['Peloton', 59.9174132, 10.7522924, 8, 'food', 'food-marker.png', 'http://facebook.com', '/infovindu-bilder/peloton.jpg'],
+    ['Munchies', 59.91638, 10.7490935, 9, 'food', 'food-marker.png', 'http://facebook.com', '/infovindu-bilder/munchies.jpg'],
+    ['Haralds Vaffler', 59.913759, 10.745212, 10, 'food', 'food-marker.png', 'http://facebook.com', '/infovindu-bilder/vafler.jpg'],
+    ['Würst', 59.9137012, 10.7465163, 11, 'food', 'food-marker.png', 'http://facebook.com', '/infovindu-bilder/wurst.jpg'],
+    ['Bari Pizza', 59.9166197, 10.7527744, 12, 'food', 'food-marker.png', 'http://facebook.com', '/infovindu-bilder/bari-pizza.jpg'],
 //
 //Bar/utested markører
 //
-    ['Cafe Sara', 59.9176068, 10.754149, 13, 'bar', 'bar-marker.png', 'http://facebook.com'],
-    ['Verkstedet', 59.9177192, 10.7517947, 14, 'bar', 'bar-marker.png', 'http://dogs.com'],
-    ['Cafe Sør', 59.9141846, 10.7461596, 15, 'bar', 'bar-marker.png', 'http://dogs.com'],
-    ['Crow bar', 59.9171999, 10.7512208, 16, 'bar', 'bar-marker.png', 'http://dogs.com'],
-    ['Tilt', 59.9162015, 10.7485173, 17, 'bar', 'bar-marker.png', 'http://dogs.com'],
-    ['Angst', 59.9139828, 10.7465088, 18, 'bar', 'bar-marker.png', 'http://dogs.com'],
-    ['Peretroska', 59.9135765, 10.7470165, 19, 'bar', 'bar-marker.png', 'http://dogs.com'],
-    ['Kniven', 59.9163927, 10.7490339, 20, 'bar', 'bar-marker.png', 'http://dogs.com'],
-    ['Oslo Bar & Bowling', 59.9158823, 10.7485441, 21, 'bar', 'bar-marker.png', 'http://dogs.com'],
-    ['Kulturhuset', 59.9146546, 10.7486177, 22, 'bar', 'bar-marker.png', 'http://dogs.com'],
+    ['Cafe Sara', 59.9176068, 10.754149, 13, 'bar', 'bar-marker.png', 'http://facebook.com', '/infovindu-bilder/cafe-sara.jpg'],
+    ['Verkstedet', 59.9177192, 10.7517947, 14, 'bar', 'bar-marker.png', 'http://dogs.com', '/infovindu-bilder/verkstedet.jpg'],
+    ['Cafe Sør', 59.9141846, 10.7461596, 15, 'bar', 'bar-marker.png', 'http://dogs.com', '/infovindu-bilder/cafe-&-sor.jpg'],
+    ['Crow bar', 59.9171999, 10.7512208, 16, 'bar', 'bar-marker.png', 'http://dogs.com', '/infovindu-bilder/crow.jpg'],
+    ['Tilt', 59.9162015, 10.7485173, 17, 'bar', 'bar-marker.png', 'http://dogs.com', '/infovindu-bilder/tilt.jpg'],
+    ['Angst', 59.9139828, 10.7465088, 18, 'bar', 'bar-marker.png', 'http://dogs.com', '/infovindu-bilder/angst.jpg'],
+    ['Peretroska', 59.9135765, 10.7470165, 19, 'bar', 'bar-marker.png', 'http://dogs.com', '/infovindu-bilder/peretroika.jpg'],
+    ['Kniven', 59.9163927, 10.7490339, 20, 'bar', 'bar-marker.png', 'http://dogs.com', '/infovindu-bilder/kniven.jpg'],
+    ['Oslo Bar & Bowling', 59.9158823, 10.7485441, 21, 'bar', 'bar-marker.png', 'http://dogs.com', '/infovindu-bilder/bar-&-bowling.jpg'],
+    ['Kulturhuset', 59.9146546, 10.7486177, 22, 'bar', 'bar-marker.png', 'http://dogs.com', '/infovindu-bilder/kulturhuset.jpg'],
 //
 //Treningssentre
 //
-    ['Actic Storgata', 59.91618200000001, 10.757546, 23, 'gym', 'gym-marker.png', 'http://dogs.com'],
-    ['Haralds Gym', 59.9157868, 10.7587026, 24, 'gym', 'gym-marker.png', 'http://dogs.com'],
-    ['SATS Schous plass', 59.9188379, 10.7603446, 25, 'gym', 'gym-marker.png', 'http://dogs.com'],
-    ['EVO Grünerløkka', 59.9199906, 10.7598709, 26, 'gym', 'gym-marker.png', 'http://dogs.com'],
-    ['SATS Spektrum', 59.9128297, 10.7549285, 27, 'gym', 'gym-marker.png', 'http://dogs.com'],
-    ['Gymmen', 59.9170116, 10.7587169, 28, 'gym', 'gym-marker.png', 'http://dogs.com'],
-    ['Lofoten Performance', 59.91722379999998, 10.7561713, 29, 'gym', 'gym-marker.png', 'http://dogs.com'],
-    ['SiO Athletica Vukan', 59.9231517, 10.7500041, 30, 'gym', 'gym-marker.png', 'http://dogs.com'],
-    ['SiO Athletica Centrum', 59.9186627, 10.731186, 31, 'gym', 'gym-marker.png', 'http://dogs.com'],
-    ['24Fitness AS', 59.91266, 10.743449, 32, 'gym', 'gym-marker.png', 'http://dogs.com'],
-    ['Fitness24Seven', 59.9138303, 10.7455569, 33, 'gym', 'gym-marker.png', 'http://dogs.com'],
-    ['SATS Akersgata', 59.914745, 10.7432694, 34, 'gym', 'gym-marker.png', 'http://dogs.com'],
-    ['Fresh Fitness St Hanshaugen', 59.925468, 10.7454316, 35, 'gym', 'gym-marker.png', 'http://dogs.com']
+    ['Actic Storgata', 59.91618200000001, 10.757546, 23, 'gym', 'gym-marker.png', 'http://dogs.com', '/infovindu-bilder/t&j.jpg'],
+    ['Haralds Gym', 59.9157868, 10.7587026, 24, 'gym', 'gym-marker.png', 'http://dogs.com', '/infovindu-bilder/t&j.jpg'],
+    ['SATS Schous plass', 59.9188379, 10.7603446, 25, 'gym', 'gym-marker.png', 'http://dogs.com', '/infovindu-bilder/t&j.jpg'],
+    ['EVO Grünerløkka', 59.9199906, 10.7598709, 26, 'gym', 'gym-marker.png', 'http://dogs.com', '/infovindu-bilder/t&j.jpg'],
+    ['SATS Spektrum', 59.9128297, 10.7549285, 27, 'gym', 'gym-marker.png', 'http://dogs.com', '/infovindu-bilder/t&j.jpg'],
+    ['Gymmen', 59.9170116, 10.7587169, 28, 'gym', 'gym-marker.png', 'http://dogs.com', '/infovindu-bilder/t&j.jpg'],
+    ['Lofoten Performance', 59.91722379999998, 10.7561713, 29, 'gym', 'gym-marker.png', 'http://dogs.com', '/infovindu-bilder/t&j.jpg'],
+    ['SiO Athletica Vukan', 59.9231517, 10.7500041, 30, 'gym', 'gym-marker.png', 'http://dogs.com', '/infovindu-bilder/t&j.jpg'],
+    ['SiO Athletica Centrum', 59.9186627, 10.731186, 31, 'gym', 'gym-marker.png', 'http://dogs.com', '/infovindu-bilder/t&j.jpg'],
+    ['24Fitness AS', 59.91266, 10.743449, 32, 'gym', 'gym-marker.png', 'http://dogs.com', '/infovindu-bilder/t&j.jpg'],
+    ['Fitness24Seven', 59.9138303, 10.7455569, 33, 'gym', 'gym-marker.png', 'http://dogs.com', '/infovindu-bilder/t&j.jpg'],
+    ['SATS Akersgata', 59.914745, 10.7432694, 34, 'gym', 'gym-marker.png', 'http://dogs.com', '/infovindu-bilder/t&j.jpg'],
+    ['Fresh Fitness St Hanshaugen', 59.925468, 10.7454316, 35, 'gym', 'gym-marker.png', 'http://dogs.com', '/infovindu-bilder/t&j.jpg']
 
 
 ];
@@ -511,12 +511,12 @@ function initMarkers() {
         marker.setVisible(false);
         google.maps.event.addListener(marker, 'mouseover', (function(marker, i){
 
-        var infoWindowContent = ('<p>Event Name: "+markers[i][0]"</p>' +
-            '<button id="bike-btn" onclick="findDirectionsFromButton('+ i +', travelModes.bike)">Sykle</button>' +
+        var infoWindowContent = ('<img id="infoImg" src=markers[i][7]>' + '<div id ="windowBtns"' +
+            '<button id="bike-btn" class="windowBtnClass" onclick="findDirectionsFromButton('+ i +', travelModes.bike)">Sykle</button>' +
             '<button id="walk-btn" onclick="findDirectionsFromButton('+ i +', travelModes.walk)">Gå</button>' +
             '<button id="drive-btn" onclick="findDirectionsFromButton('+ i +', travelModes.drive)">Miljøsvin</button>' +
             '<button id="transit-btn" onclick="findDirectionsFromButton('+ i +', travelModes.transit)">Offentlig transport</button>' +
-            '<button id="info-btn" onclick="infoLink(' + i + ')">More info</button>');
+            '<button id="info-btn" onclick="infoLink(' + i + ')">More info</button>' + '</div>');
 
             return function() {
                 infowindow.setContent(infoWindowContent);
@@ -659,6 +659,8 @@ function bysykkelStationsWithAvailability() {
         return {...station, availability: availability[station.id]}
     })
 }
+
+
 
 
 
